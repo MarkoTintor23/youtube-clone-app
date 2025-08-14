8028750d0c6437b8abf810969d8608ee32e1ed9a
Marko Tintor <marko.tintor.18@singimail.rs>
2025-08-14 17:39:37 +0200

    domaci

--- src/Components/Video.js
+++ src/Components/Video.js
@@ -1,9 +1,27 @@
 import { useParams } from "react-router-dom";
+import VIDEOS from "./../videos.json";
 
 const Video = () => {
   const { id } = useParams();
-  console.log(id);
-  return <p></p>;
+  let videoFound = null;
+
+  VIDEOS.forEach((video) => {
+    if (video.id == id) {
+      videoFound = video;
+    }
+  });
+
+  if (videoFound === null) {
+    return <h1>this video does not exist</h1>;
+  }
+
+  return (
+    <>
+      <h3>{videoFound.title}</h3>
+      <h4>{videoFound.url}</h4>
+      <iframe src={videoFound.url}></iframe>
+    </>
+  );
 };
 
 export default Video;
